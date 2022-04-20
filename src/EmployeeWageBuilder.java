16a83961c5b172375f0d28ede707904c898b8276
Madhu kahar <madhukahar@gmail.com>
2022-04-20 13:39:25 +0530

    Added: created Employee Wage

---
+++ src/EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Cpmutation");
+
+    }
+}
