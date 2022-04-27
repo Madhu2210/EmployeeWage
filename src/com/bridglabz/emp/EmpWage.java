89124d4f66c1310cd71bb67692048fb73aff3602
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-27 22:23:54 +0530

    Added: Adding Program to compute Employee Wage.

--- src/com/bridglabz/emp/EmpWage.java
+++ src/com/bridglabz/emp/EmpWage.java
@@ -1,12 +1,46 @@
 package com.bridglabz.emp;
 
 public class EmpWage {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int FULL_DAY_HR = 8;
+    public static final int PART_DAY_HR = 4;
+    public static final int EMP_WAGE_PER_HR = 20;
+    public static final int NUM_MAX_WORKING_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
+
+    static int totalWorkingDays = 0;
+    static int workingHours = 0;
+    static int totalEmpHours = 0;
+
+    public static int getWorkingHours() {
+        while (totalEmpHours < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_MAX_WORKING_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            // Calculating Wage On Employee Type
+            switch (empCheck) {
+                case 0:
+                    workingHours = FULL_DAY_HR;
+                    System.out.println("FULL DAY HR " + workingHours);
+                    break;
+                case 1:
+                    workingHours = PART_DAY_HR;
+                    System.out.println("PART DAY HR" + workingHours);
+                    break;
+                default:
+                    workingHours = 0;
+            }
+            totalEmpHours += workingHours;
+            System.out.println("Day: " + totalWorkingDays + " workingHours: " + workingHours);
+        }
+        //Calculated Employ wage
+        int totalEmpWage = totalEmpHours * EMP_WAGE_PER_HR;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
+    }
+
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        double empCheck = Math.floor (Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-        System.out.println("Employee is present");
-        else
-            System.out.println("Employee is Absent");
+        System.out.println("Welcome to Employee Wage Computation");
+        getWorkingHours();
     }
 }
